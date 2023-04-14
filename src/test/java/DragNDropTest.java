ab00d60194e46a7d3961d9d89e9c101fed7983ec
Zhansaya Kalsat <zhansayakalsat@gmail.com>
2023-04-14 22:04:03 +0600

    assertion added

--- src/test/java/DragNDropTest.java
+++ src/test/java/DragNDropTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragNDropTest {
@@ -7,5 +8,7 @@ public class DragNDropTest {
     void moveAtoBTest() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
     }
 }
